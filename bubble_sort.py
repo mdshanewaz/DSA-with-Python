349e91100b537026f4d00968611d6b8cd67ef3f0
Md Sha Newaz <sawonnstu@gmail.com>
2023-10-19 22:00:51 +0600

    new

--- bubble_sort.py
+++ bubble_sort.py
@@ -5,7 +5,7 @@ class BubbleSort:
 
         n = len(arr)
 
-        for i in range(0, n-2):
+        for i in range(0, n-1):
             for j in range(0, n-1-i):
                 if arr[j] > arr[j+1]:
                     temp = arr[j]
